480f11d39f04a2fce4f30ba790b4c1e4653b2105
kartik <karambelkarkartik209@gmail.com>
2025-07-04 14:23:07 +0530

    botly api

---
+++ botlyapi/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export async function Botly(inputMessage, botKey) {
+  try {
+    const response = await axios.post('https://yourapi.com/bot-api/botResponse',
+         { 
+            userMessage: inputMessage
+         }
+        , {
+      headers: {
+         apikey: botKey,
+      }
+    });
+
+    return response.data.data;
+  } catch (error) {
+      if (error.response) {
+      throw new Error(error.response.data?.message || 'Server returned an error');
+    } else if (error.request) {
+      throw new Error('No response from server. Please try again later.');
+    } else {
+      throw new Error(`Request error: ${error.message}`);
+    }
+  }
+}
