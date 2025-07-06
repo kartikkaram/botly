7f57b3141953aa8847a9f4832f4473ae9891102d
codexjatin <jjatinparmar54@gmail.com>
2025-07-06 13:51:05 +0530

    minor fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'botly-bot/dist/botly-bot.css';
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
